13df05e2d281c7290d28ebce48f5026a644a64c5
martin paez <martinpaez640@gmail.com>
2026-04-21 23:51:31 -0300

    se creo la funcion para quitar un nodo

--- ejercicio_2/2.c
+++ ejercicio_2/2.c
@@ -12,21 +12,91 @@ typedef struct Nodo
     Tarea T;
     struct Nodo *Siguiente;
 } Nodo;
+Nodo *crearLista();
+Nodo *crearNodo(int duracion, char descripcion[], int *tareaID);
+void InsertarNodo(Nodo **cabeza, Nodo *nodo);
+void mostrar(Nodo *l);
+Nodo *quitarNodo(Nodo **r, int dato);
 int main()
 {
+    Nodo *listaPendiente = crearLista();
+    Nodo *listaRealizada = crearLista();
+    int nueva = 0, duracion, tareaID = 1000, dato = 0;
+    char buff[60];
+    while (nueva == 0)
+    {
+        printf("\nIngrese la tarea a realizar: ");
+        gets(buff);
+
+        duracion = 10 + rand() % 91;
+        Nodo *aux = crearNodo(duracion, buff, &tareaID);
+        InsertarNodo(&listaPendiente, aux);
+        printf("\nPara ingresar una nueva tarea precione 0 para finalizar 1: ");
+        scanf("%d", &nueva);
+        getchar();
+    }
+    nueva = 0;
+
+    mostrar(listaPendiente);
+    printf("Cual de las tareas segun su id desea mover a tareas realizadas? ");
+    scanf("%d", &dato);
+    Nodo *r = quitarNodo(&listaPendiente, dato);
+    printf("\n%s", r->T.Descripcion);
+
     return 0;
+}
+Nodo *quitarNodo(Nodo **r, int dato)
+{
+    Nodo *nodoAux = *r;
+    Nodo *Auxant = NULL;
+    while (nodoAux != NULL && dato != nodoAux->T.TareaID)
+    {
+        Auxant = nodoAux;
+        nodoAux = nodoAux->Siguiente;
+    }
+    if (nodoAux != NULL)
+    {
+        if (nodoAux == *r)
+        {
+            *r = nodoAux->Siguiente;
+        }
+        else
+        {
+            Auxant->Siguiente = nodoAux->Siguiente;
+        }
+        nodoAux->Siguiente = NULL;
+    }
+    return nodoAux;
+}
+void mostrar(Nodo *l)
+{
+    Nodo *aux = l;
+    int i = 1;
+    while (aux != NULL)
+    {
+        printf("\nTarea numero: %d", i);
+        printf("\nDescripcion de la tarea: %s", aux->T.Descripcion);
+        printf("\nId de la tarea: %d", aux->T.TareaID);
+        i++;
+        aux = aux->Siguiente;
+    }
 }
 Nodo *crearLista()
 {
     return NULL;
 }
-Nodo *crearNodo(int tareaID, char descripcion[], int *duracion)
+Nodo *crearNodo(int duracion, char descripcion[], int *tareaID)
 {
     Nodo *aux = (Nodo *)malloc(sizeof(Nodo));
-    aux->T.TareaID = tareaID;
+    aux->T.Duracion = duracion;
     aux->T.Descripcion = (char *)malloc(sizeof(char) * (strlen(descripcion) + 1));
     strcpy(aux->T.Descripcion, descripcion);
-    aux->T.Duracion = *duracion;
-    (*duracion)++;
+    aux->T.TareaID = *tareaID;
+    (*tareaID)++;
     return aux;
+}
+void InsertarNodo(Nodo **cabeza, Nodo *nodo)
+{
+    nodo->Siguiente = *cabeza;
+    *cabeza = nodo;
 }
